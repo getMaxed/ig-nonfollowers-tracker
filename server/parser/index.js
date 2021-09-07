7d13fc8affc2b8884b7a73b6f1432c1d4452bff1
getMaxed <maxfaden07@gmail.com>
2021-09-06 21:10:44 -0700

    Re-starting point

--- server/parser/index.js
+++ server/parser/index.js
@@ -4,6 +4,74 @@ const _difference = require('lodash/difference');
 
 util.inspect.defaultOptions.maxArrayLength = null;
 
+
+
 (async () => {
-    console.log('working');
+    const followers = getProfiles('followers');
+    const following = getProfiles('following');
+
+    const target = _difference(following, followers);
+    console.log({ 'Non Followers List': target });
+
+    fs.writeFileSync(
+        'non-followers',
+        target.map((u, idx) => `${idx + 1}. ${u}`).join('\n')
+    );
 })();
+
+/*
+|--------------------------------------------------------------------------
+| HELPERS
+|--------------------------------------------------------------------------
+*/
+
+function getProfiles(type) {
+    const START_OF_NEW_PROF = 's profile picture';
+    const entries = fs
+        .readFileSync(`./parser/${type}`)
+        .toString()
+        .split('\n')
+        .filter(s => s.indexOf('Verified') === -1);
+
+    const profiles = [];
+    let n = 0;
+
+    console.log({ entries });
+
+    while (n !== entries.length) {
+        const asdfg = entries[n + 2].slice(0, 18).replace(/\n|\r/g, '');
+        console.log({ asdfg });
+
+        const profileRows =
+            entries[n + 2].slice(-18).replace(/\n|\r/g, '') ===
+            START_OF_NEW_PROF
+                ? 2
+                : 3;
+
+        console.log({ profileRows });
+
+        if (n === 0) {
+            profiles.push(
+                entries[n + 1] &&
+                    entries[n + 1].slice(-18).replace(/\n|\r/g, '')
+            );
+        }
+
+        n = n + profileRows;
+        profiles.push(
+            entries[n + 1] && entries[n + 1].slice(-18).replace(/\n|\r/g, '')
+        );
+    }
+
+    consoleLogWithEnd(`${type.toUpperCase()} count: ${profiles.length}`);
+    return profiles.filter(x => !!x);
+}
+
+function consoleLogWithEnd(...lines) {
+    lines.forEach(l => console.log(l));
+
+    console.log('.....................................');
+    console.log('.....................................');
+    console.log('.....................................');
+    console.log('.....................................');
+}
