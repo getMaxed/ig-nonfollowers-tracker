6139bc3bf4681fd8f4de860b20c524cb1562d0ab
getMaxed <maxfaden07@gmail.com>
2021-09-06 23:42:55 -0700

    Parse list anew

--- server/parser/index.js
+++ server/parser/index.js
@@ -6,6 +6,8 @@ util.inspect.defaultOptions.maxArrayLength = null;
 
 (async () => {
     const followers = getProfiles('followers');
+    return;
+
     const following = getProfiles('following');
 
     const target = _difference(following, followers);
@@ -24,46 +26,74 @@ util.inspect.defaultOptions.maxArrayLength = null;
 */
 
 function getProfiles(type) {
-    return console.log('get profiles');
+    // return console.log('get profiles');
     const START_OF_NEW_PROF = 's profile picture';
     const entries = fs
         .readFileSync(`./parser/${type}`)
         .toString()
         .split('\n')
-        .filter(s => s.indexOf('Verified') === -1);
+        .filter(s => s !== '·\r')
+        .filter(s => s.indexOf('Verified') === -1); // trg: handle THIS
+
+    // console.log({ entries, entriesLen: entries.length });
+    // return;
 
     const profiles = [];
-    let n = 0;
+    let currProfile = [];
 
-    console.log({ entries });
-
-    while (n !== entries.length) {
-        const asdfg = entries[n + 2].slice(0, 18).replace(/\n|\r/g, '');
-        console.log({ asdfg });
-
-        const profileRows =
-            entries[n + 2].slice(-18).replace(/\n|\r/g, '') ===
-            START_OF_NEW_PROF
-                ? 2
-                : 3;
-
-        console.log({ profileRows });
-
-        if (n === 0) {
-            profiles.push(
-                entries[n + 1] &&
-                    entries[n + 1].slice(-18).replace(/\n|\r/g, '')
-            );
+    for (let i = 0; i < entries.length; i++) {
+        // if (i % 3 === 0) {
+        if (entries[i].indexOf(START_OF_NEW_PROF) > 0) {
+            profiles.push(currProfile);
+            currProfile = [entries[i]];
+        } else {
+            currProfile.push(entries[i]);
         }
-
-        n = n + profileRows;
-        profiles.push(
-            entries[n + 1] && entries[n + 1].slice(-18).replace(/\n|\r/g, '')
-        );
     }
 
-    consoleLogWithEnd(`${type.toUpperCase()} count: ${profiles.length}`);
-    return profiles.filter(x => !!x);
+    // console.log(profiles, profiles.length);
+
+    profiles.forEach(async (x, idx) => {
+        (function(idx) {
+            setTimeout(function() {
+                console.log('value is ', x);
+            }, 3000 * (idx + 1));
+        })(idx);
+    });
+
+    return;
+
+    // let n = 0;
+
+    // console.log({ entries });
+
+    // while (n !== entries.length) {
+    //     const asdfg = entries[n + 2].slice(0, 18).replace(/\n|\r/g, '');
+    //     console.log({ asdfg });
+
+    //     const profileRows =
+    //         entries[n + 2].slice(-18).replace(/\n|\r/g, '') ===
+    //         START_OF_NEW_PROF
+    //             ? 2
+    //             : 3;
+
+    //     console.log({ profileRows });
+
+    //     if (n === 0) {
+    //         profiles.push(
+    //             entries[n + 1] &&
+    //                 entries[n + 1].slice(-18).replace(/\n|\r/g, '')
+    //         );
+    //     }
+
+    //     n = n + profileRows;
+    //     profiles.push(
+    //         entries[n + 1] && entries[n + 1].slice(-18).replace(/\n|\r/g, '')
+    //     );
+    // }
+
+    // consoleLogWithEnd(`${type.toUpperCase()} count: ${profiles.length}`);
+    // return profiles.filter(x => !!x);
 }
 
 function consoleLogWithEnd(...lines) {
